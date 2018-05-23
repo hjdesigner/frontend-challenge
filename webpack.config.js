7029695ac11f5fe68b9ad5289a8814234add4908
Henrique Melanda <hjdesigner23@gmail.com>
2018-05-23 20:20:57 -0300

    COMMIT INITIAL
    Basis of the project:
    - NodeJs to run the API
    - Webpack Babel to transpile the ES6
    - Eslint for code quality
    - Mocha with Chai for testing
    
    Created route / fields for API access
    Created route / to show the generated form based on the API

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+// webpack v4
+const path = require('path');
+module.exports = {
+  entry: { main: './src/view/scripts/app.js' },
+  output: {
+    path: path.resolve(__dirname, './src/view/js'),
+    filename: 'app.js'
+  },
+  module: {
+		rules: [
+			{
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
