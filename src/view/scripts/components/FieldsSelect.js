8e9131c6dfc911828b41650349ac97b40858dcb0
Henrique Melanda <hjdesigner23@gmail.com>
2018-05-23 22:12:18 -0300

    COMPONENTS FIELDS SELECT
    Created the component that mounts the select view.
    This component receives two parameters, one is a json with the information that will contain it and the other is the HTML where it will be inputted.
    This component has validation whether it is mandatory or not.
    
    The tests created for it are:
    - should FieldsSelect exit
    - Should FieldsSelect is a function
    - Should create and append the markup give a correct data1 and markup1
    - Should create and append the markup give a correct data2 and markup2

---
+++ src/view/scripts/components/FieldsSelect.js
@@ -0,0 +1,11 @@
+export default function FieldsSelect(data, element) {
+	const elementHTML = element;
+	const item = data;
+	const markup = `<div class="form__field">
+		<label for="${item.name}" class="form__label">${item.label}</label>
+		<select id="${item.name}" name="${item.name}" class="form__input" ${item.required === true ? 'required="required"' : ''}>
+			${Object.keys(item.values).map(option => `<option value="${item.values[option]}">${option}</option>\n`).join('')}
+		</select>
+	</div>`;
+	elementHTML.insertAdjacentHTML('beforeend', markup);
+}
---
+++ tests/SelectFields.spec.js
@@ -0,0 +1,83 @@
+import 'jsdom-global/register';
+import chai, { expect } from 'chai';
+import FieldsSelect from '../src/view/scripts/components/FieldsSelect.js';
+
+describe('FieldSelect', () => {
+	describe('Some tests', () => {
+		it('should FieldsSelect exit', () => {
+			expect(FieldsSelect).to.exist;
+		});
+		it('Should FieldsSelect is a function', () => {
+			expect(FieldsSelect).to.be.a('function');
+		});
+	});
+
+	const data = {
+		"name": "Qual será o serviço?",
+    "label": "Qual será o serviço?",
+    "placeholder": "Qual será o serviço?",
+    "mask": "tipo de serviço",
+    "type": "enumerable",
+  	"required": true,
+    "values": {
+    	"Coloração": "Coloração",
+    	"Corte": "Corte",
+    	"Escova ": "Escova ",
+    	"Escova progressiva/definitiva": "Escova progressiva/definitiva",
+    	"Luzes": "Luzes",
+    	"Manicure": "Manicure",
+    	"Pedicure": "Pedicure",
+    	"Penteado": "Penteado"
+    }
+	}
+	const data2 = {
+    "name": "O serviço será para quantas pessoas?",
+    "label": "O serviço será para quantas pessoas?",
+    "placeholder": "O serviço será para quantas pessoas?",
+    "mask": "indique o número de pessoas",
+    "type": "enumerable",
+    "required": false,
+    "values": {
+     	"1": "1",
+      "2": "2",
+      "Mais de 3": "Mais de 3"
+    }
+  }
+
+	const markup = `<div class="form__field">
+		<label for="Qual será o serviço?" class="form__label">Qual será o serviço?</label>
+		<select id="Qual será o serviço?" name="Qual será o serviço?" class="form__input" required="required">
+			<option value="Coloração">Coloração</option>
+<option value="Corte">Corte</option>
+<option value="Escova ">Escova </option>
+<option value="Escova progressiva/definitiva">Escova progressiva/definitiva</option>
+<option value="Luzes">Luzes</option>
+<option value="Manicure">Manicure</option>
+<option value="Pedicure">Pedicure</option>
+<option value="Penteado">Penteado</option>
+
+		</select>
+	</div>`;
+
+	const markup2 = `<div class="form__field">
+		<label for="O serviço será para quantas pessoas?" class="form__label">O serviço será para quantas pessoas?</label>
+		<select id="O serviço será para quantas pessoas?" name="O serviço será para quantas pessoas?" class="form__input">
+			<option value="1">1</option>
+<option value="2">2</option>
+<option value="Mais de 3">Mais de 3</option>
+
+		</select>
+	</div>`;
+
+	it('Should create and append the markup give a correct data1 and markup1', () => {
+		const element = document.createElement('div');
+		FieldsSelect(data, element);
+		expect(element.innerHTML).to.be.eql(markup);
+	});
+
+	it('Should create and append the markup give a correct data2 and markup2', () => {
+    const element2 = document.createElement('div');
+    FieldsSelect(data2, element2);
+    expect(element2.innerHTML).to.be.eql(markup2);
+  });
+})
